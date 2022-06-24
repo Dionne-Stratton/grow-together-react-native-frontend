63773ed2ddf2e771e9fbb65419e0ebe2e87e7e90
Dionne <mylife33@hotmail.com>
2022-06-24 00:52:34 -0500

    Added in textinput box and installed some more necessary dependencies.

--- src/screens/authScreens/SignupScreen.js
+++ src/screens/authScreens/SignupScreen.js
@@ -1,34 +1,35 @@
 import React, { useContext, useState } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native'
 import CheckBox from 'expo-checkbox'
 
 const SignupScreen = ({ navigation }) => {
 
     const [checked, setChecked] = useState(false)
+    const [text, onChangeText] = useState('')
 
     return (
         <View style={styles.container}>
-        <Text h3 style={{marginBottom: 25}} >Sign Up here</Text>
-        <View style={styles.flexRow}>
-            <Text>I am a mentor: </Text>
-            <CheckBox
-                disabled={false}
-                value={checked}
-                onValueChange={(newValue) => setChecked(newValue)}
+            <Text h3 style={{marginBottom: 25}} >Sign Up here</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={onChangeText}
+                value={text}
+                placeholder="Name"
             />
-        </View>
-        <TouchableOpacity onPress={() => navigation.navigate('Signin')} >
-            <Text style={styles.link} >Sign In instead.</Text>
-        </TouchableOpacity>
+            <View style={styles.flexRow}>
+                <Text>I am a mentor: </Text>
+                <CheckBox
+                    disabled={false}
+                    value={checked}
+                    onValueChange={(newValue) => setChecked(newValue)}
+                />
+            </View>
+            <TouchableOpacity onPress={() => navigation.navigate('Signin')} >
+                <Text style={styles.link} >Sign In instead.</Text>
+            </TouchableOpacity>
         </View>
         )
 }
-
-// SignupScreen.navigationOptions = () => {
-//     return {
-//       headerShown: false,
-//     };
-// };
 
 const styles = StyleSheet.create({
     container: {
@@ -44,7 +45,13 @@ const styles = StyleSheet.create({
     flexRow: {
         display: 'flex',
         flexDirection: 'row'
-      }
+    },
+    input: {
+    height: 40,
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 15
+    },
 })
 
 export default SignupScreen
