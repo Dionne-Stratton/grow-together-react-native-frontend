bf330082fc54b9dc0418c3a00e5828073dbaf7c3
Dionne Stratton <mylife33@hotmail.com>
2022-06-25 23:01:21 -0500

    added some inputs and made some styling changes

--- src/screens/authScreens/SigninScreen.js
+++ src/screens/authScreens/SigninScreen.js
@@ -1,36 +1,71 @@
-import React, { useContext, useState } from 'react'
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native'
+import React, { useContext, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 
-const SigninScreen = ({navigation}) => {
+const SigninScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    return (
-        <View style={styles.container}>
-        <Text style={{marginBottom: 25}} >Sign In</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Signup')} >
-            <Text style={styles.link} >Sign Up instead.</Text>
-        </TouchableOpacity>
-        </View>
-        )
-}
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Sign In</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setEmail}
+        value={email}
+        placeholder="Email"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+        placeholder="Password"
+      />
+      <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
+        <Text style={styles.link}>Sign Up instead.</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 SigninScreen.navigationOptions = () => {
-    return {
-      headerShown: false,
-    };
+  return {
+    headerShown: false,
+  };
 };
 
 const styles = StyleSheet.create({
-    container: {
-        margin: 15,
-        flex: 1,
-        justifyContent: 'center',
-        marginTop: 70
-    },
-    link: {
-        color: '#5ea303',
-        marginTop: 30,
-        fontSize: 16
-    }
-})
+  container: {
+    margin: 15,
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 70,
+  },
+  link: {
+    color: "#5ea303",
+    marginTop: 30,
+    fontSize: 20,
+  },
+  flexRow: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  input: {
+    height: 50,
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 15,
+    fontSize: 20,
+  },
+  text: {
+    fontSize: 30,
+    marginBottom: 15,
+  },
+});
 
-export default SigninScreen
+export default SigninScreen;
--- src/screens/authScreens/SignupScreen.js
+++ src/screens/authScreens/SignupScreen.js
@@ -1,57 +1,82 @@
-import React, { useContext, useState } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native'
-import CheckBox from 'expo-checkbox'
+import React, { useContext, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import CheckBox from "expo-checkbox";
 
 const SignupScreen = ({ navigation }) => {
+  const [checked, setChecked] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [checked, setChecked] = useState(false)
-    const [text, onChangeText] = useState('')
-
-    return (
-        <View style={styles.container}>
-            <Text h3 style={{marginBottom: 25}} >Sign Up here</Text>
-            <TextInput
-                style={styles.input}
-                onChangeText={onChangeText}
-                value={text}
-                placeholder="Name"
-            />
-            <View style={styles.flexRow}>
-                <Text>I am a mentor: </Text>
-                <CheckBox
-                    disabled={false}
-                    value={checked}
-                    onValueChange={(newValue) => setChecked(newValue)}
-                />
-            </View>
-            <TouchableOpacity onPress={() => navigation.navigate('Signin')} >
-                <Text style={styles.link} >Sign In instead.</Text>
-            </TouchableOpacity>
-        </View>
-        )
-}
+  return (
+    <View style={styles.container}>
+      <Text h3 style={styles.text}>
+        Sign Up here
+      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setEmail}
+        value={email}
+        placeholder="Email"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+        placeholder="Password"
+      />
+      <View style={styles.flexRow}>
+        <Text style={{ fontSize: 20 }}>I am a mentor: </Text>
+        <CheckBox
+          disabled={false}
+          style={styles.checkBox}
+          value={checked}
+          onValueChange={(newValue) => setChecked(newValue)}
+        />
+      </View>
+      <TouchableOpacity onPress={() => navigation.navigate("Signin")}>
+        <Text style={styles.link}>Sign In instead.</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        margin: 15,
-        flex: 1,
-        justifyContent: 'center'
-    },
-    link: {
-        color: '#5ea303',
-        marginTop: 30,
-        fontSize: 16
-    },
-    flexRow: {
-        display: 'flex',
-        flexDirection: 'row'
-    },
-    input: {
-    height: 40,
+  container: {
+    margin: 15,
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 70,
+  },
+  link: {
+    color: "#5ea303",
+    marginTop: 30,
+    fontSize: 20,
+  },
+  flexRow: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  input: {
+    height: 50,
     borderWidth: 1,
     padding: 10,
-    marginBottom: 15
-    },
-})
+    marginBottom: 15,
+    fontSize: 20,
+  },
+  text: {
+    fontSize: 30,
+    marginBottom: 15,
+  },
+  checkBox: {
+    height: 30,
+    width: 30,
+  },
+});
 
-export default SignupScreen
+export default SignupScreen;
